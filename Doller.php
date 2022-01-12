f4d73f7177364043411538037955e8f93441cd4a
shivam <shivamgorakhpur44@gmail.com>
2022-01-12 09:31:14 +0530

    Initial commit

---
+++ Doller.php
@@ -0,0 +1,9 @@
+<?php  
+$x = "abc";  
+$$x = 200;  
+echo $x."<br/>";  
+echo $$x."<br/>";  
+echo $abc;  
+?> 
+
+
---
+++ printvsecho.php
@@ -0,0 +1,17 @@
+<?php
+ 
+ $x=15;  
+ $X=30;  
+ $z=$x+$X;  
+ echo "Sum: ",$z;  
+ // here $x and $X are two different varuabale
+ // therefore it doesn't give any error.
+ 
+ 
+ ?>
+ <!-- /*echo "<h2>PHP is Fun!</h2>";
+ echo "Hello world!<br>";
+ echo "I'm about to learn PHP!<br>";
+ print "<h2>PHP is Fun!</h2>";
+ print "Hello world!<br>";
+ print "I'm about to learn PHP!";*/ -->
